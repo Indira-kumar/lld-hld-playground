22d15b29e846f7f9dcdffe7205164a2f518fc4ab
indira-kumar <indirakumarak@gmail.com>
2026-09-15 10:10:10 +0530

    <snakes-ladder>: add play game with optimized special character lookup

--- snakes-and-ladders/README.md
+++ snakes-and-ladders/README.md
@@ -9,5 +9,4 @@
 - The positions of entities/special characters are not given by the client, but are decided randomly by the app at the start of the game.
 Condition: the combination of positions of different characters must be valid. Snake head, and ladder tail cant be at the same location.
 - Every player will have multiple pawns/pieces on the board.
-- When will game start for a player: when they roll 1.
-- When will game end for a player: when they reach board end
+- When will game end for a player: when one of their pieces reaches board end
--- snakes-and-ladders/snake_and_ladder.py
+++ snakes-and-ladders/snake_and_ladder.py
@@ -1,6 +1,7 @@
 from random import randint
 
 from .models import *
+from .dice_strategy import DefaultDiceStrategy
 
 def create_game() -> Game:
     no_of_players = _get_valid_count_helper('no. of players', 2)
@@ -15,32 +16,55 @@ def create_game() -> Game:
         players.append(player)
 
     dims = _get_valid_count_helper('dimension of board', 5)
-    board = Board(n=dims)
-    pieces = []
+    pieces = {}
     for i, player in enumerate(players):
         for j in range(no_of_pieces):
             piece = Piece(id=(i*no_of_players + j), color=player.color, position=Position(-1,-1), player=player)
-            pieces.append(piece)
+            pieces[player.name] = [piece] if not pieces[player.name] else pieces[player.name].append(piece)
     print("\nPieces Initialized")
 
     no_of_snakes = randint(1, dims)
     no_of_frogs = randint(1, dims)
     no_of_ladders = randint(1, dims)
     snakes = []
-    for i in range(no_of_snakes):
+    special_chars = {}
+    cnt = 0
+    while(cnt < no_of_snakes):
         start_pos = Position(x= randint(1, dims), y=randint(1, dims))
         end_pos = Position(x= randint(1, dims), y=randint(1, start_pos.y))
-        snakes.append(Snake(start_pos=start_pos, end_pos=end_pos))
+        snake = Snake(start_pos=start_pos, end_pos=end_pos)
+        if not special_chars[start_pos.x][start_pos.y]:
+            snakes.append(snake)
+            special_chars[start_pos.x][start_pos.y] = snake
+            cnt += 1
+        else:
+            continue
     frogs = []
-    for i in range(no_of_frogs):
+    cnt = 0
+    while(cnt < no_of_frogs):
         start_pos = Position(x= randint(1, dims-1), y=randint(1, dims-1))
         end_pos = Position(x= randint(1, dims), y=randint(start_pos.y, dims))
-        frogs.append(Frog(start_pos=start_pos, end_pos=end_pos))
+        frog = Frog(start_pos=start_pos, end_pos=end_pos)
+        if not special_chars[start_pos.x][start_pos.y]:
+            frogs.append(frog)
+            special_chars[start_pos.x][start_pos.y] = frog
+            cnt += 1
+        else:
+            continue
     ladders = []
-    for i in range(no_of_ladders):
+    cnt = 0
+    while(cnt < no_of_ladders):
         start_pos = Position(x= randint(1, dims-1), y=randint(1, dims-1))
         end_pos = Position(x= randint(1, dims-1), y=randint(start_pos.y, dims))
-        ladders.append(Ladder(start_pos=start_pos, end_pos=end_pos))
+        ladder = Ladder(start_pos=start_pos, end_pos=end_pos)
+        if not special_chars[start_pos.x][start_pos.y]:
+            ladders.append(ladder)
+            special_chars[start_pos.x][start_pos.y] = ladder
+            cnt += 1
+        else:
+            continue
+
+    board = Board(n=dims, special_chars=special_chars)
 
     return Game(players=players, pieces=pieces, board=board, snakes=snakes, frogs=frogs, ladders=ladders, no_of_pieces=no_of_pieces)
 
@@ -52,7 +76,35 @@ def _get_valid_count_helper(var_name: str, min_count: int):
     return var
 
 def play_game(game:Game):
-    print("Game Started")
+    print("\nGame Started")
+    dice = DefaultDiceStrategy()
+    game_won = False
+    while(not game_won):
+        for i in game.players:
+            number = dice.roll_dice()
+            print("\nRolling the dice for: ", i.name)
+            print(f"\nYou got: {number}")
+            choice = input("\nChoose which piece to move: ", game.pieces[i.name])
+            move_piece(game, game.pieces[i.name][choice], number)
+            game_won = _is_game_won()
+
+
+def move_piece(game: Game, piece: Piece, move_count: int):
+    dim = game.board.n
+    curr_pos = piece.position
+    game.board.positions[curr_pos.x][curr_pos.y].remove(piece)
+    x = move_count % dim
+    y = move_count // dim
+    if game.board.special_chars[x][y]:
+        x = game.board.special_chars[x][y].end_position.x
+        y = game.board.special_chars[x][y].end_position.y
+
+    piece.position = Position(x, y)
+    game.board.positions[x][y].append(piece)
+    print("\nPiece moved")
+
+def _is_game_won() -> bool:
+    pass
 
 
 def __main__():
